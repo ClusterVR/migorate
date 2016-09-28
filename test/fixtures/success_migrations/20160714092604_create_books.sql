8c0d997df85ef83bafdaa46658bdebe48dcf0cde
Michiaki Mizoguchi <michiaki.mizoguchi@gmail.com>
2016-09-28 09:00:21 +0900

    Make test data multiple line statement

--- test/fixtures/success_migrations/20160714092604_create_books.sql
+++ test/fixtures/success_migrations/20160714092604_create_books.sql
@@ -1,6 +1,10 @@
 -- +migrate Up
-CREATE TABLE books(id INT PRIMARY KEY AUTO_INCREMENT, title VARCHAR(255), author VARCHAR(255), created_at TIMESTAMP);
-
+CREATE TABLE books (
+  id         INT PRIMARY KEY AUTO_INCREMENT,
+  title      VARCHAR(255),
+  author     VARCHAR(255),
+  created_at TIMESTAMP
+);
 
 -- +migrate Down
 DROP TABLE books;
--- test/fixtures/success_migrations/20160716102604_create_authors.sql
+++ test/fixtures/success_migrations/20160716102604_create_authors.sql
@@ -1,10 +1,21 @@
 -- +migrate Up
-CREATE TABLE authors(id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255), created_at TIMESTAMP);
-ALTER TABLE books DROP COLUMN author;
-ALTER TABLE books ADD(author_id INT NOT NULL);
-ALTER TABLE books ADD CONSTRAINT fk_books_author_id FOREIGN KEY(author_id) REFERENCES authors(id);
+CREATE TABLE authors (
+  id         INT PRIMARY KEY AUTO_INCREMENT,
+  name       VARCHAR(255),
+  created_at TIMESTAMP
+);
+
+ALTER TABLE books
+  DROP COLUMN author;
+
+ALTER TABLE books
+  ADD (author_id INT NOT NULL);
+
+ALTER TABLE books
+  ADD CONSTRAINT fk_books_author_id FOREIGN KEY (author_id) REFERENCES authors (id);
 
 -- +migrate Down
 DROP TABLE authors;
-ALTER TABLE books ADD (author VARCHAR(255));
+ALTER TABLE books
+  ADD (author VARCHAR(255));
 
